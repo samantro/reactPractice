2c7d3726f4daccc42a9a89e07558f8563093f024
samantro <66985251+samantro@users.noreply.github.com>
2022-07-15 11:05:17 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>My favourait games</h1>
+    <ul>
+      <li>Football</li>
+      <li>Basketball</li>
+      <li>Pool</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
